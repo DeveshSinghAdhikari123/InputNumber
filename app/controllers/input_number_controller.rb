a6e3898b4088e238b8cf958c82a15b5247212a1c
DeveshSinghAdhikari123 <devesh.adhikari123@gmail.com>
2024-05-15 11:12:06 +0530

    convert the given input  to the desired output

--- app/controllers/input_number_controller.rb
+++ app/controllers/input_number_controller.rb
@@ -1,11 +1,26 @@
 class InputNumberController < ApplicationController
 
 def input_op_number
+	num =  params[:input_text]
+	@arr = []
+	num.each_char do|str|
+	 @arr << number_sum(str)
+	end
 
 end
 
 def index
 end
+
+
+private
+ 
+ def number_sum(number_string)
+  return if !number_string.to_i
+   number = number_string.split(",")
+   total_sum = number.map(&:to_i).sum
+    return total_sum
+ end
 
 
 end
